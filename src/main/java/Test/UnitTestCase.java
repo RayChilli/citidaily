29ddfd346be598fabdec93f3ac83cf348d4db33e
Raymond <raychen79@gmail.com>
2025-06-02 16:34:13 -0400

    Testcommit

--- src/main/java/Test/UnitTestCase.java
+++ src/main/java/Test/UnitTestCase.java
@@ -5,4 +5,14 @@ package Test;
 
  */
 public class UnitTestCase {
+
+    @RestRequestMapping("/test")
+    public String test() {
+        // This method is a placeholder for a unit test case.
+        // It simulates a REST endpoint that can be tested.
+        return "Unit Test Case Executed";
+    }
+
+    @GetMapping("/test");
+
 }
---
+++ src/main/java/ca/tdi/App.java
@@ -0,0 +1,16 @@
+package ca.tdi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+        System.out.println( "Git branch 01" );
+    }
+}
